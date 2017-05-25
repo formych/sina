1ba217deaba903e7175fabf9adceb4afb6d3ab3a
ruifeng <renrufiengch@gmail.com>
2017-05-25 17:46:11 +0800

    change run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 file=$1
-sed -i '/新闻标题/d'  $1
-sed -i '/总计/d'  $1
+sed -i '/新闻标题：/d'  $1
+sed -i '/总计：/d'  $1
 sed -i '/评论:/d'  $1
-sed -i '/原文链接/d'  $1
+sed -i '/原文链接：/d'  $1
 sed -i 's/\t//g'  $1
 
